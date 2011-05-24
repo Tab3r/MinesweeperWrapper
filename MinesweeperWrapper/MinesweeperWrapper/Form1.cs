6128186f864f42752f20f27f3a2acf9670dd427d
David Tabernero <david.tabernero@gmail.com>
2011-05-24 11:13:51 +0200

    Initial commit

---
+++ MinesweeperWrapper/MinesweeperWrapper/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace MinesweeperWrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.ShowInTaskbar = false;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void LaunchMineweeper()
+        {
+            bool openlog = false;
+
+            StreamWriter sw = null;
+            
+            try
+            {
+                sw = new StreamWriter(@"c:\ASkLog.txt", true);
+                sw.WriteLine("INIT: " + DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString());
+                sw.Flush();
+
+                openlog = true;
+            }
+            catch
+            {
+                openlog = false;
+                sw = null;
+            }
+
+            string dirSystem = Environment.GetEnvironmentVariable("SystemRoot");
+
+            ProcessStartInfo psi = new ProcessStartInfo(dirSystem + @"\system32\winmine.exe");
+            Process pr = Process.Start(psi);
+            pr.WaitForExit();
+
+            if (openlog)
+            {
+                sw.WriteLine("END: " + DateTime.Now.ToLongDateString() + " - " + DateTime.Now.ToLongTimeString());
+                sw.Flush();
+                sw.Close();
+            }
+
+            
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            LaunchMineweeper();
+            this.Close();
+        }
+    }
+}
